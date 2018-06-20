c42c8d7112ab772ea59010978e34e0ccf80de70f
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-06-20 13:11:47 +0300

    adding some general settings

--- app.js
+++ app.js
@@ -19,8 +19,37 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
 // Routing Config
 app.use('/categories', categoriesRoute);
 app.use('/articles', articlesRoute);
+
+// If none of the above routes match
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// Global Error Handling
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 module.exports = app;
